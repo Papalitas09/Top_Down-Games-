3c4fddd9b778fa3ff94d61c176f6dd3550ca1dc6
Papalitas09 <miaw.raja@gmail.com>
2024-08-07 20:08:05 +0700

    first commit

---
+++ Scripts/Char.gd
@@ -0,0 +1,123 @@
+extends CharacterBody2D
+
+@onready var animation = $AnimatedSprite2D
+const speed = 100
+var moving: bool = false
+var dir: String = "None"
+@onready var mobile = $Tombol_Gerak
+@onready var tombol = $pencet
+
+
+func _ready():
+	pass 
+
+
+
+func _process(delta):
+	move()
+	anim()
+	
+
+	
+
+		
+	
+	
+
+	
+		
+	
+	
+	
+	
+
+	
+
+func move():
+	#pc/etc
+	if Input.is_action_pressed("Right"):
+		velocity = Vector2.RIGHT * speed 
+		moving = true
+		
+		dir = "Right"
+	elif Input.is_action_pressed("Left"):
+		velocity = Vector2.LEFT * speed 
+		moving = true
+		
+		dir = "Left"
+	elif Input.is_action_pressed("Down"):
+		velocity = Vector2.DOWN * speed 
+		moving = true
+		
+		dir = "Down"
+	elif Input.is_action_pressed("Top"):
+		velocity = Vector2.UP * speed 
+		moving = true
+	
+		dir = "Top"
+		
+	#Mobile
+	elif Input.is_action_pressed("Right_Mobile"):
+		velocity = Vector2.RIGHT * speed 
+		moving = true
+		dir = "Right"
+	elif Input.is_action_pressed("Left_Mobile"):
+		velocity = Vector2.LEFT * speed 
+		moving = true
+		dir = "Left"
+	elif Input.is_action_pressed("Down_Mobile"):
+		velocity = Vector2.DOWN * speed 
+		moving = true
+		dir = "Down"
+	elif Input.is_action_pressed("Top_Mobile"):
+		velocity = Vector2.UP * speed 
+		moving = true
+		dir = "Top"
+		
+	
+	else :
+		velocity = Vector2.ZERO
+		moving = false
+		
+		
+	
+	
+		
+		
+	move_and_slide()
+		
+func anim():
+	#Jika moving true maka, animation nya walk
+	if moving == true :
+		match dir:
+			"Right":
+				animation.play("Side_Walk")
+				animation.flip_h = false
+			"Left":
+				animation.play("Side_Walk")
+				animation.flip_h = true
+			"Down":
+				animation.play("Down_Walk")
+			"Top":
+				animation.play("Top_Walk")
+			
+	#Jika movingnya false, maka jadi idle	
+	elif moving == false:
+		match dir:
+			"Right":
+				animation.play("Side_Idle")
+				animation.flip_h = false
+			"Left":
+				animation.play("Side_Idle")
+				animation.flip_h = true
+			"Down":
+				animation.play("Down_Idle")
+			"Top":
+				animation.play("Top_Idle")
+				
+			
+	
+	move_and_slide()
+		
+
+
---
+++ Scripts/tombol;.gd
@@ -0,0 +1,22 @@
+extends Node2D
+@onready var mobile = $"../Tombol_Gerak"
+var kalikali = 0
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("pencet") and kalikali == 0:
+		mobile.visible = true
+		kalikali = 1
+	elif Input.is_action_just_pressed("pencet") and kalikali == 1:
+		mobile.visible = false
+		kalikali = 0
+	
+		
+	
+		
+	
+	pass
