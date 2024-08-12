702d1b27cf6616057003114764bffd53b41bdc1e
Papalitas09 <miaw.raja@gmail.com>
2024-08-12 19:34:22 +0700

    Nanti fix bugs buat coin sat

--- Scripts/ItemsCollect.gd
+++ Scripts/ItemsCollect.gd
@@ -1,6 +1,8 @@
 extends Area2D
 @onready var label = $Control/Label
 var mc_masuk = false
+@onready var count = $"../../MC/Label"
+var coins:int
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,7 +11,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if mc_masuk == true and Input.is_action_just_pressed("Take_Items"):
-		queue_free()
+		coins += 1
+		count.text = str(coins)
+#nanti fix coins bugsnya
+		
 	pass
 
 
